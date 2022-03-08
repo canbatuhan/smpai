4176593dc39c50efb5aa8b968a81e8094402870c
ituitis-canb18 <canbatuhan.official@gmail.com>
2022-03-08 11:19:37 +0300

    modules are taken from config

--- tests/pyfile_as_argument/main_with_arg.py
+++ tests/pyfile_as_argument/main_with_arg.py
@@ -7,10 +7,13 @@ func_name = 'sum'
 def generate_command(module_name, func_name, *args):
     return f"""python -c "from {module_name} import {func_name}; print({func_name}({args}))"""
 
-if __name__=="__main__":
-    value = subprocess.run(
-        args=generate_command(module_name, func_name),
+def run_command(command):
+    return subprocess.run(
+        args=command,
         capture_output=True,
         shell=True,
         universal_newlines=True
     ).stdout
+
+if __name__=="__main__":
+    result = run_command(generate_command(module_name, func_name))
---
+++ tests/pyfile_as_argument/main_with_config.py
@@ -0,0 +1,36 @@
+import json
+import os
+import subprocess
+
+
+json_file = 'resources/pyfile.json'
+
+def generate_command(module_name, func_name, *args):
+    return f"""python -c "from {module_name} import {func_name}; print({func_name}({args}))"""
+
+def run_command(command):
+    return float(subprocess.run(
+        args=command,
+        capture_output=True,
+        shell=True,
+        universal_newlines=True
+    ).stdout)
+
+
+if __name__=="__main__":
+    functions = json.load(open(json_file, "r"))
+    
+    sum_func = functions["sum_func"]
+    mul_func = functions["mul_func"]
+
+    sum_cmd = generate_command(sum_func["module"], sum_func["function"])
+    mul_func = generate_command(mul_func["module"], mul_func["function"])
+
+    sum_result = run_command(sum_cmd)
+    mul_result = run_command(mul_func)
+
+    print("Sum: {}".format(sum_result))
+    print("Mul: {}".format(mul_result))
+
+    final_result = sum_result + mul_result
+    print("Result: {}".format(final_result))
