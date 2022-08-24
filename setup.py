1d07860ea1ec1bb3bdd80ff15a16d0ff73315737
ituitis-canb18 <canbatuhan.official@gmail.com>
2022-08-24 20:14:04 +0300

    setup time!

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='sm-py',
+    version='0.1.0a0',    
+    description='smpy - A State Machine Framework',
+    url='https://github.com/canbatuhan/sm-py',
+    author='Batuhan Can',
+    author_email='batuhanosmancan@gmail.com',
+    license='MIT',
+    packages=['smpy'],
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',       
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Typing :: Typed',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS'
+    ],
+)
--- smpy/__init__.py
+++ smpy/__init__.py
@@ -1,1 +1,5 @@
 from .fsm import FiniteStateMachine
+
+__version__ = "0.1.0-alpha"
+__author__ = 'Batuhan Can'
+__credits__ = 'Büşra Asan, Kutay Acar'
