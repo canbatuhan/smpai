966314794b2539ceac1d4467bd5c7702a030d2bd
ituitis-canb18 <canbatuhan.official@gmail.com>
2022-09-02 11:26:41 +0300

    version fix

--- smpai/__init__.py
+++ smpai/__init__.py
@@ -2,6 +2,6 @@ from . import builder
 from . import components
 from .fsm import FiniteStateMachine
 
-__version__ = "0.1.1-alpha"
+__version__ = "0.1.2-alpha"
 __author__ = 'Batuhan Can'
 __credits__ = 'Büşra Asan, Kutay Acar'
